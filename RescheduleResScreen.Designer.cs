61b8848d31af8d78c44d05ee1728ec0290982008
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2021-06-23 03:32:32 +0530

    Reschedule screen update

--- RescheduleResScreen.Designer.cs
+++ RescheduleResScreen.Designer.cs
@@ -30,7 +30,150 @@ namespace airline_reservation_system
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RescheduleResScreen));
+            this.textBoxRID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.seatNumber = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxRID
+            // 
+            this.textBoxRID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRID.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxRID.Location = new System.Drawing.Point(178, 32);
+            this.textBoxRID.Name = "textBoxRID";
+            this.textBoxRID.Size = new System.Drawing.Size(404, 24);
+            this.textBoxRID.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(19, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 23);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Reservation ID:";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.datePicker.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datePicker.Location = new System.Drawing.Point(177, 158);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(405, 24);
+            this.datePicker.TabIndex = 20;
+            // 
+            // seatNumber
+            // 
+            this.seatNumber.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.seatNumber.DropDownHeight = 50;
+            this.seatNumber.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seatNumber.FormattingEnabled = true;
+            this.seatNumber.IntegralHeight = false;
+            this.seatNumber.Items.AddRange(new object[] {
+            "A1",
+            "A2",
+            "A3",
+            "A4",
+            "A5",
+            "A6",
+            "B1",
+            "B2",
+            "B3",
+            "B4",
+            "B5",
+            "B6",
+            "C1",
+            "C2",
+            "C3",
+            "C4",
+            "C5",
+            "C6",
+            "D1",
+            "D2",
+            "D3",
+            "D4",
+            "D5",
+            "D6",
+            "E1",
+            "E2",
+            "E3",
+            "E4",
+            "E5",
+            "E6",
+            "F1",
+            "F2",
+            "F3",
+            "F4",
+            "F5",
+            "F6"});
+            this.seatNumber.Location = new System.Drawing.Point(178, 204);
+            this.seatNumber.Name = "seatNumber";
+            this.seatNumber.Size = new System.Drawing.Size(404, 27);
+            this.seatNumber.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(18, 208);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 23);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "New Seat Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(18, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 23);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "New Date:";
+            // 
+            // validateButton
+            // 
+            this.validateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.validateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(30)))), ((int)(((byte)(120)))));
+            this.validateButton.FlatAppearance.BorderSize = 0;
+            this.validateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.validateButton.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validateButton.ForeColor = System.Drawing.Color.White;
+            this.validateButton.Location = new System.Drawing.Point(20, 80);
+            this.validateButton.Name = "validateButton";
+            this.validateButton.Size = new System.Drawing.Size(560, 35);
+            this.validateButton.TabIndex = 21;
+            this.validateButton.Text = "Validate Reservation";
+            this.validateButton.UseVisualStyleBackColor = false;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.updateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(30)))), ((int)(((byte)(120)))));
+            this.updateButton.FlatAppearance.BorderSize = 0;
+            this.updateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateButton.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateButton.ForeColor = System.Drawing.Color.White;
+            this.updateButton.Location = new System.Drawing.Point(20, 269);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(560, 35);
+            this.updateButton.TabIndex = 22;
+            this.updateButton.Text = "Update Reservation";
+            this.updateButton.UseVisualStyleBackColor = false;
             // 
             // RescheduleResScreen
             // 
@@ -38,13 +181,31 @@ namespace airline_reservation_system
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
             this.ClientSize = new System.Drawing.Size(601, 351);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.seatNumber);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxRID);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RescheduleResScreen";
             this.Text = "Reschedule Reservation";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxRID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.ComboBox seatNumber;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button validateButton;
+        private System.Windows.Forms.Button updateButton;
     }
 }
