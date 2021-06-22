ad6f00a3bccaeafa6f85bd223af6c6d3aaaf2360
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2021-06-23 02:27:45 +0530

    Schedule reservation screen UI update

--- RescheduleResScreen.Designer.cs
+++ RescheduleResScreen.Designer.cs
@@ -29,13 +29,16 @@ namespace airline_reservation_system
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RescheduleResScreen));
             this.SuspendLayout();
             // 
             // RescheduleResScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(30)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(601, 351);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RescheduleResScreen";
             this.Text = "Reschedule Reservation";
             this.ResumeLayout(false);
